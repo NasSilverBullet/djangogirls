2103e3ed1c9f4beba8fb68b89ce96d6948088df0
y.taniguchi <y.softvalley@gmail.com>
2019-01-03 01:28:40 +0900

    reset settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,13 +124,18 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # herokuapp
 
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+​
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+​
+ALLOWED_HOSTS = ['*']
+​
+STATIC_ROOT = 'staticfiles'
+​
 DEBUG = False
-
+​
 try:
     from .local_settings import *
 except ImportError:
     pass
-
-if not DEBUG:
-    import django_heroku
-    django_heroku.settings(locals())
